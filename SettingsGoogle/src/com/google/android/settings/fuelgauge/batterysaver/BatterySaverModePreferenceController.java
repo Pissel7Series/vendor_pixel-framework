281e57426b4cc7f94b76ebc828470849db86f20d
Pyrtle93 <xxpyrtlexx93@gmail.com>
2025-11-27 22:35:14 +0530

    r

--- SettingsGoogle/src/com/google/android/settings/fuelgauge/batterysaver/BatterySaverModePreferenceController.java
+++ SettingsGoogle/src/com/google/android/settings/fuelgauge/batterysaver/BatterySaverModePreferenceController.java
@@ -14,7 +14,7 @@ import android.view.View;
 import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceScreen;
 import com.android.internal.annotations.VisibleForTesting;
-import com.android.internal.util.infinity.InfinityUtils;
+import com.android.internal.util.mist.Utils;
 import com.android.settings.core.BasePreferenceController;
 import com.android.settingslib.core.lifecycle.LifecycleObserver;
 import com.android.settingslib.core.lifecycle.events.OnPause;
@@ -38,7 +38,7 @@ public class BatterySaverModePreferenceController extends BasePreferenceControll
 
     @Override 
     public int getAvailabilityStatus() {
-        boolean available = InfinityUtils.isPackageInstalled(mContext, "com.google.android.flipendo", false);
+        boolean available = Utils.isPackageInstalled(mContext, "com.google.android.flipendo", false);
         return available ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
     }
 
